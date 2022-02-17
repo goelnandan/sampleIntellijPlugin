ec2f002a5f3a0f6ef8691bc044ed631eee22a909
GitHub Action <action@github.com>
2022-02-17 14:55:43 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/goelnandan/sampleintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.goelnandan.sampleintellijplugin.services
+
+import com.github.goelnandan.sampleintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
